6eee02c975ec1309d8f48bd1345184ef1c81b67b
Elyar Abuduxun <Abuduxun@gmail.com>
2024-10-09 11:46:07 +0200

    nieuw folder

--- arrangementen.php
+++ arrangementen.php
@@ -6,68 +6,6 @@
     <title>Arrangementen</title>
 </head>
 <body>
-    <main>
-        <div id="cookingar">
-        <img id="cooking" href="img/Live_cooking_buffet.png">
-        <p>
-            Altijd al eens een kijkje bij ons ‘in de keuken’ willen nemen? Dat kan, we brengen de keuken gewoon het restaurant in!
-            <br><br>
-            Tijdens het live cooking buffet maken onze koks à la minute heerlijke gerechten voor je 
-            klaar met verse ingrediënten van het seizoen. Ideaal wanneer je bijvoorbeeld een 
-            personeelsuitje, teamuitje of familiedag hebt in Friesland. Wij bieden het live cooking 
-            buffet aan voor groepen vanaf 40 personen.
-            <br><br>
-            Neem voor meer informatie of een offerte op maat contact met ons op!
-        </p>
-        </div>
-        <div id="bierar">
-        <img id="bier" href="img/Bier_arrangementen.png">
-        <p>
-            De dag gezellig afsluiten of de avond goed beginnen met vrienden, of wil je je zakelijke 
-            borrel nét wat extra’s geven? Dan zit je bij ons gebeiteld!
-            <br><br>
-            Tijdens het bierarrangement serveren wij vier speciaalbieren die wij voor jou 
-            selecteren met daarbij diverse bijpassende gerechtjes. Bij iedere ronde krijg je een 
-            korte uitleg over de specifieke kenmerken van het betreffende bier en daarna laten we 
-            je gewoon weer met rust ;
-            <br><br>
-            Je kunt genieten van het Bierarrangement bij zowel De Walrus Leeuwarden als De 
-            Walrus Sneek.. Reserveer voor €42,50 per persoon of neem voor meer informatie 
-            contact met ons op.
-            <br><br>
-            De Walrus Leeuwarden
-            Tel: 058 - 21 377 40
-            E-mail: info@dewalrusleeuwarden.nl
-            Reserveer direct bij De Walrus Leeuwarden
-            <br><br>
-            De Walrus Sneek
-            Tel: 0515 - 438 100
-            E-mail: info@dewalrussneek.nl
-            Reserveer direct bij De Walrus Sneek
-        </p>
-        </div>
-        <div  id="theaterar">
-        <img id="theater" href="img/Theater_arrangementen.png">
-        <p>
-            Maak je theateravond compleet met het 3-gangen theatermenu bij De Walrus Sneek.
-            <br><br>
-            Geniet van het Theatermenu bij Grand-Café De Walrus Sneek. Op vertoon van je 
-            theaterkaartje geniet je van een exclusief 3-gangen menu, de perfecte start van een 
-            avond vol entertainment. Uiteraard zorgen wij ervoor dat je op tijd bij de voorstelling 
-            bent. 
-            <br><br>
-            Geef in het opmerkingenveld bij je reservering 'theater sneek' aan, en dan zorgen wij 
-            dat het goed komt. Je kunt natuurlijk ook binnenlopen zonder te reserveren.
-            <br><br>
-            Reserveren
-            <br><br>
-            HET MENU
-            <br><br>
-            Het Theater Menu bestaat uit 3-gangen en kost 34,50 per persoon. 
-            <br><br>
-            Bekijk het menu
-        </p>
-        </div>
-    </main>
+    
 </body>
 </html>
