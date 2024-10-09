6e3eec03793e209ad507b062cda524cae18b795a
Elyar Abuduxun <Abuduxun@gmail.com>
2024-10-09 11:50:46 +0200

    css naam aangepast

--- hoome.php
+++ hoome.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link REL="stylesheet" href="home.css">
+  <link REL="stylesheet" href="hoome.css">
   <title>De Walrus</title>
 </head>
 <body>
