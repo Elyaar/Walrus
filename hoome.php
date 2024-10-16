09f5661c33b29aace2dbbd07bb2b8f5d071bd7d7
Ibrahim-web-arch <ibrahimgoed@gmail.com>
2024-10-16 14:59:21 +0200

    div is nu echt gefixed

--- hoome.php
+++ hoome.php
@@ -14,11 +14,12 @@
 
   ?>
 
-<div class=div-header> 
 
-</div>
+  <div class=div-header>
 
- 
+  </div>
+
+
   <main>
     <div id="content_top">
       <img id="KO" src="img/walrus1.png" alt="foto1">
