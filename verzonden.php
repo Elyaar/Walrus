766ea3a9e741e578dc61a9380fefc930d1253494
the-program-boss <python.mrk@gmail.com>
2024-10-23 10:35:45 +0200

    verzonden pagina toegevoegd

---
+++ verzonden.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>verzonden</title>
+    <link rel="stylesheet" href="verzonden.css">
+</head>
+
+<body>
+    <div class="text-1">
+        <h1>uw reactie is opgeslagen</h1> <br>
+        <h2>en uw reactie is aangekomen</h2> <br>
+        <h2>bedankt voor de contact en we gaan er zo snel mogelijk mee aan de gang
+
+        </h2> <br>
+    </div>
+</body>
+
+</html>
