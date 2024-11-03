209ee145de47d30469103a1fd8bc6bfa935bdca2
the-program-boss <python.mrk@gmail.com>
2024-11-03 14:02:16 +0100

    the login front page

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+    <div class="login-container">
+        <img src="img/De walrus.png" height="150" width="250" alt="">
+        <h2>Login</h2>
+        <p>met je gebruikersnaam en je wachtwoord</p>
+        <form>
+            <input type="text" placeholder="gebruikersnaam" required>
+            <input type="password" placeholder="wachtwoord" required>
+            <button type="submit">Login</button>
+        </form>
+        <p>Don't have an account? <a href="#">Sign up</a></p>
+    </div>
+
+</body>
+
+</html>
