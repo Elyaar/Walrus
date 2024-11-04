8376b8627f7aaf7e65abe5222fcc15316303a56c
the-program-boss <python.mrk@gmail.com>
2024-11-04 14:01:53 +0100

    echo veranderd

--- dbh.inc.php
+++ dbh.inc.php
@@ -8,5 +8,5 @@ try {
     $pdo = new PDO($dsn, $dbusername, $dbpassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "je domme sukkel het werkt niet ga het fixen nu dan: " . $e->getMessage();
+    echo "Error gevonden ->: " . $e->getMessage();
 }
