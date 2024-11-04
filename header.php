bf1b19bdbb331a6811bf5ef766d6858230beb7fd
MK-v5 <mksomna@st.noorderpoort.nl>
2024-11-04 13:35:07 +0100

    small changes

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Carter+One&display=swap" rel="stylesheet">
-  <link REL="stylesheet" href="home.css">
+  <link rel="stylesheet" href="home.css">
   <title>De Walrus</title>
 </head>
 
