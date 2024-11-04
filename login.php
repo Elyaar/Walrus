e64fbb561da2bd42f400daaa2130f262cc7641c7
the-program-boss <python.mrk@gmail.com>
2024-11-04 14:48:20 +0100

    n

--- login.php
+++ login.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>Login Pagina</title>
     <link rel="stylesheet" href="login.css">
 </head>
 
 <body>
-    <div class="login-container">
+    <div class="login">
         <img src="img/De walrus.png" height="150" width="250" alt="">
         <h2>Login</h2>
         <p>met je gebruikersnaam en je wachtwoord</p>
@@ -18,7 +18,7 @@
             <input type="password" placeholder="wachtwoord" required>
             <button type="submit">Login</button>
         </form>
-        <p>Don't have an account? <a href="#">Sign up</a></p>
+        <p>heb je geen account? <a href="#">maak er een!</a></p>
     </div>
 
 </body>
